d97f7cb5eb800d401282eab721ac184eca0bf24b
Ivan Korolev <zerkc_97@mail.ru>
2025-04-18 12:07:02 +0000

    Identify test inside logs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -63,4 +63,10 @@ RSpec.configure do |config|
 
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.before do |example|
+    Rails.logger.debug { "-" * 50 }
+    Rails.logger.debug { example.full_description }
+    Rails.logger.debug { example.location }
+  end
 end
