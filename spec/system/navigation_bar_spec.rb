9834e44192e386d08336780cbaf0f124d33f50a7
Ivan Korolev <zerkc_97@mail.ru>
2025-04-21 09:03:12 +0000

    Add navigation for authenticated user

--- spec/system/navigation_bar_spec.rb
+++ spec/system/navigation_bar_spec.rb
@@ -1,10 +1,36 @@
 require "system_helper"
 
 RSpec.describe "Navigation bar" do
-  it "contains `Sign up` and `Sign in` buttons" do
-    visit root_path
+  context "when user is not signed in" do
+    before { Rails.configuration.x.user_signed_in = false }
 
-    expect(navbar).to have_link("Sign up")
-    expect(navbar).to have_link("Sign in")
+    it "contains `Sign up` and `Sign in` buttons" do
+      visit root_path
+
+      expect(navbar).to have_link("Sign up")
+      expect(navbar).to have_link("Sign in")
+    end
+  end
+
+  context "when user is signed in" do
+    before do
+      Rails.configuration.x.user_signed_in = true
+      visit root_path
+    end
+
+    it "contains a menu of application sections" do
+      expect(navbar).to have_link("Capital")
+      expect(navbar).to have_link("Accounts")
+      expect(navbar).to have_link("Transactions")
+      expect(navbar).to have_link("Categories")
+    end
+
+    it "contains user name" do
+      expect(navbar).to have_content("User")
+    end
+
+    it "contains `Sign out` button" do
+      expect(navbar).to have_link("Sign out")
+    end
   end
 end
