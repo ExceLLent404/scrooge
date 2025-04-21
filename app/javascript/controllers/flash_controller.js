5c3ef07efb3afb8c2d4b819191ef563e51b08c32
Ivan Korolev <zerkc_97@mail.ru>
2025-04-21 11:08:41 +0000

    Limit time to display notification

--- app/javascript/controllers/flash_controller.js
+++ app/javascript/controllers/flash_controller.js
@@ -1,6 +1,12 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  connect() {
+    const timeout = this.element.classList.contains("is-success") ? 3000 : 10000;
+
+    setTimeout(() => this.remove(), timeout);
+  }
+
   remove() {
     this.element.remove()
   }
