18ee31041915528cb5293914660dac2c9b695752
Ivan Korolev <zerkc_97@mail.ru>
2025-04-29 08:52:13 +0000

    Use normalized user email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  normalizes :email, with: ->(email) { email.strip.downcase }
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
