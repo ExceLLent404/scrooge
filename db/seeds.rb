04d6f6c84230a962ca27f92a9d1613fb57d13de1
Ivan Korolev <zerkc_97@mail.ru>
2025-04-30 06:59:42 +0000

    Add demo user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,5 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+unless Rails.env.test? || User.exists?(email: ENV.fetch("DEMO_USER_EMAIL"))
+  include FactoryBot::Syntax::Methods # rubocop:disable Style/MixinUsage
+
+  create(:user, email: ENV.fetch("DEMO_USER_EMAIL"), password: ENV.fetch("DEMO_USER_PASS"))
+end
